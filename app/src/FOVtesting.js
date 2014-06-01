903184246a9c4cd25d29641a3c013431e1b812a1
redwoodfavorite <joseph.michael.sample@gmail.com>
2014-06-01 00:35:34 -0700

    added culling example

---
+++ app/src/FOVtesting.js
@@ -0,0 +1,82 @@
+define(function(require, exports, module) {
+    var Engine           = require('famous/core/Engine');
+    var Modifier         = require('famous/core/Modifier');
+    var Surface          = require('famous/core/Surface');
+    var CulledSurface    = require('famous/core/CulledSurface');
+    var Transform        = require('famous/core/Transform');
+    var Easing           = require('famous/transitions/Easing');
+    var Transitionable   = require('famous/transitions/Transitionable');
+
+    var context = Engine.createContext();
+    context.setPerspective(1000);
+
+    document.body.style.backgroundColor = 'black';
+
+    // This number represents the number of pixels that are added to
+    // the side of the culled viewport.  It is negative in this case
+    // to illustrate the culling effect.
+
+    var cullingPadding = -150;
+
+    // console.log("viewport angle is: ", Math.atan((window.innerWidth/2)/1000) * 57.2957795, " degrees")
+
+    /////////////////////////////////////////////////////
+    // INITIALIZE MOVING SURFACES
+    /////////////////////////////////////////////////////
+
+    var mods = [];
+    for (var i = 0; i < 50; i++) {
+        var surf = new CulledSurface({
+            size: [75, 75],
+            cullingPadding: cullingPadding,
+            properties: {
+                backgroundColor: 'lightgreen'
+            }
+        });
+        var mod = new Modifier({
+            size: [74, 74],
+            origin: [0.5, 0.5],
+            align: [0.5, 0.5],
+            transform: Transform.translate(0, 0, -500)
+        });
+
+        moveRandom(mod, i);
+
+        context.add(mod).add(surf)
+    };
+
+    /////////////////////////////////////////////////////
+    // FUNCTION FOR CONTINUOS RANDOM MOVEMENT
+    /////////////////////////////////////////////////////
+
+    function moveRandom(mod, ind) {
+        var randomZ = -Math.random() * 4000;
+        var randomX = (Math.random() * 5000) - 2500;
+        var randomY = (Math.random() * 5000) - 2500;
+
+        mod.setTransform(Transform.translate(
+            randomX, randomY, randomZ),
+                {
+                    duration: Math.random() * 2000 + 3000
+                },
+                moveRandom.bind(null, mod, ind)
+            );
+    }
+
+    /////////////////////////////////////////////////////
+    // INITIALIZE RENDER BOX
+    /////////////////////////////////////////////////////
+
+    var renderBox = new Surface({
+        size: [window.innerWidth + (cullingPadding * 2), window.innerHeight + (cullingPadding * 2)],
+        properties: {
+            color: 'white',
+            border: '2px dotted white'
+        }
+    });
+    var renderBoxMod = new Modifier({
+        origin: [0.5, 0.5]
+    });
+
+    context.add(renderBoxMod).add(renderBox);
+});
